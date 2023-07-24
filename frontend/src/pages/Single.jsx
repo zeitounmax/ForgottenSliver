7448570f1864112946b890a5d4c6c9628eb37d67
zeitounmax <mthiry1@gmail.com>
2023-07-24 14:46:32 +0200

    minorUpdate

--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -30,8 +30,28 @@ function Single() {
         </div>
         <h1>Bonjour Ceci est un texte</h1>
         <p>
-          Il est beau le texte.Sinon ça va c'est la dernière semaine et je suis
-          assez confiant pour la suite
+          Allez parait qu'il faut marquer un truc. ça doit faire 5 mois que j'ai
+          débuté cette formation. C'est beaucoup de trajet en train , je dois
+          être facile à du 50k km au moins.
+        </p>
+        <p>
+          Mais j'ai pu apprendre des choses, comme que j'aime ce métier. Certes
+          j'en chie mais aussi j'apprends des trucs et ça me donne d'apprendre
+          et évoluer.
+        </p>
+        <p>
+          Mais ça va, j'aurai aimé avoir plus de jours pour améliorer ce
+          checkpoint mais je vais pas m'en faire car je vais pouvoir l'améliorer
+          et tenter d'intégrer des choses plus complexe.
+        </p>
+        <p>
+          C'était pas évident cette méthode d'apprentisage, mais je suis rester
+          là . .Car j'ai envie de réussir. . Maintenant je vais pouvoir dormir
+          car j'en pouvais plus du trajet.
+        </p>
+        <p>
+          Merci Romain et Justine pour la patience et cet envie d'apprendre des
+          trucs.. Sincérement Merci .. Max
         </p>
       </div>
       <Menu />
